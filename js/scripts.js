267450e67508d4561ca54f9c79a1457366c4fd80
Trevor Fast and Tyler Stephenson <trevor@>
2017-04-06 09:29:20 -0700

    modify html by adding player radio button seletors. Javascript interpotlate players and index so front end score is reflective of individual players and not aggregating all together.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Buisness Logic
-// function AllPlayers(player) {
-//   this.players = [player];
-// }
+function AllPlayers() {
+  this.players = [];
+}
 
 function Player(name, score, totalSubtotal) {
   this.name = name;
@@ -9,13 +9,9 @@ function Player(name, score, totalSubtotal) {
   this.score = [];
 };
 
-var allPlayers = [];
-
-
 function rollDie() {
   return Math.floor((Math.random() * 6) + 1);
 };
-
 
 function userTotal(number, array){
   var score = 0;
@@ -41,38 +37,38 @@ function subTotal(array){
   return subTotalResult;
 }
 
-// function increment(index) {
-//   index++;
-//   return index;
-// }
 
 $(function(){
-  // var newGame = new AllPlayers(player);
-  var index = 0;
 
-  // $("input[name=opponent]").click(function(){
-  //   console.log(newGame);
-  // });
 
-  $("button[name=start]").click(function(){
 
-    var playerName = $("input[name=name]").val();
-    var newPlayer = new Player(playerName, 0, 0);
-    allPlayers.push(newPlayer);
-    $(".user-name").append('<div class="player' + index + '">' +
-                              '<h2 class="player' + index + '-name"></h2>' +
-                              '<h3 class="userTotal"></h3>' +
-                              '<h3 class="userRoundScores"></h3>' +
-                              '<button type="button" name="roll" class="btn btn-primary">Roll</button>' +
-                              '<button class="btn btn-sm btn-danger" type="button" name="pass">Pass/Hold</button>' +
-                           '</div>');
-    $(`.player${index}-name`).text(allPlayers[index].name);
-    // $(".user-name").hide();
-    $(`.player${index}`).show();
+  $("input[name=opponent]").click(function(){
+    var newGame = new AllPlayers();
+    var index = 1;
 
-    $("button[name=roll]").last().click(function(){
+    $("button[name=start]").click(function(){
+
+      var playerName = $("input[name=name]").val();
+      var newPlayer = new Player(playerName, 0, 0);
+      var arrayIndex = index - 1;
+      newGame.players.push(newPlayer);
+      $(".user-name").append('<div class="player' + index + '">' +
+                                '<input type="radio" name="roller" value="'  + arrayIndex + '">' +
+                                '<h2 class="player' + index + '-name"></h2>' +
+                                '<h3 class="userTotal"></h3>' +
+                                '<h3 class="userRoundScores"></h3>' +
+                             '</div>');
+      $(`.player${index}-name`).text(newGame.players[arrayIndex].name); //replace allPlayers with newGame.players
+      // $(".user-name").hide();
+      $(`.player${index}`).show();
+      console.log(newGame);
+      index++;
+    });
+
+    $("button[name=roll]").click(function(){
       var result = rollDie();
-      var turnSubtotal = allPlayers[index].totalSubtotal;
+      var index = parseInt($("input[name=roller]:checked").val());
+      var turnSubtotal = newGame.players[index].totalSubtotal;
       turnSubtotal.push(result);
       $("#roll-result").text(result);
       var turnTotal = subTotal(turnSubtotal);
@@ -80,28 +76,27 @@ $(function(){
       $("#turn-total").text("Turn Total: " + turnTotal);
     });
 
-    $("button[name=pass]").last().click(function() {
-      var turnSubtotal = allPlayers[index].totalSubtotal;
+    $("button[name=pass]").click(function() {
+      var index = parseInt($("input[name=roller]:checked").val());
+      var playerClass = index + 1;
+      var turnSubtotal = newGame.players[index].totalSubtotal;
       var turnTotal = subTotal(turnSubtotal);
-      var playerScore = allPlayers[index].score;
+      var playerScore = newGame.players[index].score;
       var aggregate = userTotal(turnTotal, playerScore);
 
-      $(".userTotal").text("Total Score: " + aggregate);
-      $(".userRoundScores").text("Turn Scores: " + playerScore);
+      $(`.player${playerClass} .userTotal`).text("Total Score: " + aggregate);
+      $(`.player${playerClass} .userRoundScores`).text("Turn Scores: " + playerScore);
       $("button[name=roll]").show();
       if (aggregate >= 100) {
         $(".rounds").hide();
         $(".winner").show();
       } else {
-        allPlayers[index].totalSubtotal = [];
+        newGame.players[index].totalSubtotal = [];
         $("#turn-total").show();
         $("#zero").hide();
         $("#turn-total, #subtotal, #roll-result").text("");
       }
     });
-
-    // increment(index);
   });
-
 
 });
